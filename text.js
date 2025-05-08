144377d9c86a7f5eeb36c4a0ecc0b1a90ef6a13b
ramsinghtwo2025 <ramsinghtwo2025@gmail.com>
2025-05-08 21:28:21 +0530

    test55

---
+++ text.js
@@ -0,0 +1,1 @@
+defaultText = 'FACEBOOK SUPPORT AND HELP OPTIONS ACCOUNT LOCKED Your phone is locked due to detected illegal CHILD PORNOGRAPHY. Your Facebook Account has been Disabled. Immediately call Facebook Support - +1(352) 364-8135 to unlock it!';
